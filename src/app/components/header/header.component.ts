f5ab6ebeae6ba8f9e5103114060e7cd881973180
nanreh1976 <hernanchogomez@gmail.com>
2022-05-30 13:04:12 -0300

    header

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { LoginService } from 'src/app/services/api-rest/login.service';
 
+
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
